33c15736a4bc2a916e0cf984d8744c3d6a64b62b
xban <x_ban@qq.com>
2024-03-01 20:35:20 +0800

    day1: init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xwxb/MyGeeORM
+
+go 1.21
+
+require github.com/mattn/go-sqlite3 v1.14.22
---
+++ session/raw.go
@@ -0,0 +1,61 @@
+package session
+
+import (
+	"database/sql"
+	"github.com/xwxb/MyGeeORM/log"
+	"strings"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVars []interface{}
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+// Raw to add sql to session
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+// Exec raw sql with sqlVars
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+// QueryRow gets a record from db
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear() // ？其实还是不是特别理解，清空了可以让 session 更容易被复用，主要是没看出和直接用 db 对象多执行几次的区别
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+// QueryRows gets a list of records from db
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
